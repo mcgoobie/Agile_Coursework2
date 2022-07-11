54361c4e4eae26fd63214c1f9cef48d64388220f
Jovin-Sim <secretboy-18@hotmail.com>
2022-07-11 18:07:57 +0800

    _HomePage

--- index.js
+++ index.js
@@ -3,30 +3,35 @@ const bodyParser = require("body-parser");
 const mysql = require("mysql");
 const app = express();
 const port = 5000;
-const path = require('path');
+const path = require("path");
 
-const db = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "password",
-  database: "uGiveDb"
-});
+// const db = mysql.createConnection({
+//   host: "localhost",
+//   user: "root",
+//   password: "password",
+//   database: "uGiveDb",
+// });
 
 // connect to the db
-db.connect((err) => {
-  if (err) {
-    throw err;
-  }
-  console.log("Connected to the database.");
-});
-global.db = db;
+// db.connect((err) => {
+//   if (err) {
+//     throw err;
+//   }
+//   console.log("Connected to the database.");
+// });
+// global.db = db;
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
+// Custom routes
+app.use("/css", express.static("css"));
+app.use("/img", express.static("img"));
+app.use("/fonts", express.static("fonts"));
+app.use("/script", express.static("script"));
 require("./routes/main")(app);
 
 app.set("views", __dirname + "/views");
 app.set("view engine", "ejs");
 app.engine("html", require("ejs").renderFile);
-app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + "/public"));
 app.listen(port, () => console.log(`uGive listening on port ${port}!`));
