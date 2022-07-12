c8abc164412d005b482899841e78af81ea2f977d
Jovin-Sim <secretboy-18@hotmail.com>
2022-07-12 16:59:48 +0800

    _HomePage_Updated Banner Image Issue #1

--- index.js
+++ index.js
@@ -3,24 +3,23 @@ const bodyParser = require("body-parser");
 const mysql = require("mysql");
 const app = express();
 const port = 5000;
-const path = require('path');
+const path = require("path");
 
-
-// const db = mysql.createConnection({
-//   host: "localhost",
-//   user: "root",
-//   password: "password",
-//   database: "uGiveDb",
-// });
+const db = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "",
+  database: "uGiveDb",
+});
 
 // connect to the db
-// db.connect((err) => {
-//   if (err) {
-//     throw err;
-//   }
-//   console.log("Connected to the database.");
-// });
-// global.db = db;
+db.connect((err) => {
+  if (err) {
+    throw err;
+  }
+  console.log("Connected to the database.");
+});
+global.db = db;
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
@@ -34,5 +33,5 @@ require("./routes/main")(app);
 app.set("views", __dirname + "/views");
 app.set("view engine", "ejs");
 app.engine("html", require("ejs").renderFile);
-app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + "/public"));
 app.listen(port, () => console.log(`uGive listening on port ${port}!`));
